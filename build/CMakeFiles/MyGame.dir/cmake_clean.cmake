1dde51f8a2f5c9f8e3111ed5d3a8e8983615e2c9
Labid-svg <nazmulhassanlabid@gmail.com>
2026-01-18 09:04:39 +0600

    Add files via upload

---
+++ build/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/main.cpp.o"
+  "CMakeFiles/MyGame.dir/main.cpp.o.d"
+  "CMakeFiles/MyGame.dir/space_shooter.cpp.o"
+  "CMakeFiles/MyGame.dir/space_shooter.cpp.o.d"
+  "MyGame"
+  "MyGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
